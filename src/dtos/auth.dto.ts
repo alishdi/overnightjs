742ecbbee02533515d783eca940a41b8654d7d89
Ali Shahidi <alishahidi267@gmail.com>
2023-12-01 21:29:33 +0330

    use packge overnight.js

---
+++ src/dtos/auth.dto.ts
@@ -0,0 +1,36 @@
+
+// export interface RegisterDTO {
+//     username: string;
+//     fullname: string;
+//     password: string;
+// }
+
+import { Expose } from "class-transformer";
+import { IsDefined, Matches,  } from "class-validator";
+
+export class RegisterDTO {
+    @IsDefined()
+    @Expose()
+    @Matches(RegExp(/[A-Za-z0-9\_\.]{5,20}/))
+    username: string;
+    @IsDefined()
+    @Expose()
+    password: string; 
+    @IsDefined()
+    @Expose()
+    @Matches(RegExp(/[\w\s]{5,35}/))
+    fullname: string;
+    @IsDefined()
+    @Expose()
+    @Matches(RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/))
+    email: string;
+}
+export class LoginDTO {
+    @IsDefined()
+    @Expose()
+    @Matches(RegExp(/[A-Za-z0-9\_\.]{5,20}/))
+    username: string;
+    @IsDefined()
+    @Expose()
+    password: string; 
+}
